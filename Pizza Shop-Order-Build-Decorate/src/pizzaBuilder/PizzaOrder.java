f7caf76c209c55cee877a2610f45eed83c927668
GiseleDab <60444486+GiseleDab@users.noreply.github.com>
2020-04-30 23:38:38 -0500

    Gisele's Pizza

--- Pizza Shop-Order-Build-Decorate/src/pizzaBuilder/PizzaOrder.java
+++ Pizza Shop-Order-Build-Decorate/src/pizzaBuilder/PizzaOrder.java
@@ -37,6 +37,12 @@ public class PizzaOrder{
     p = pb.pizzas(1).build();
     reportOrder();
   }
+  public void bakeGiselePizza(){
+      System.out.format("Baking an incredible Gisele pizza...\n");
+      pb = new Pizza.Builder("Gisele Pizza");
+      p = pb.pizzas(1).build();
+      reportOrder();
+  }
 
   private void reportOrder() {
       System.out.format("\nBuilt %s - num pies: %d, pepperoni: %b, onion: %b, frenchfries %b\n", 
